cc3024aa56ea3201b072eff2f4cad97f70174181
Lucria <jerryzhang28eclipse@gmail.com>
2020-04-05 16:02:31 +0800

    [Feat] Did two questions on leetcode

---
+++ 412.fizz-buzz.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=412 lang=cpp
+ *
+ * [412] Fizz Buzz
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result;
+        for (int i = 1; i < n+1; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                result.push_back("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.push_back("Fizz");
+            } else if (i % 5 == 0) {
+                result.push_back("Buzz");
+            } else {
+                result.push_back(to_string(i));
+            }
+        }
+        return result;
+    }
+};
+// @lc code=end
+
