b6dd260f0d733d0504ccd6e3afdc529a4929bc1c
Lucria <jerryzhang28eclipse@gmail.com>
2021-01-20 08:54:45 +0800

    Recursive Inorder Traversal

---
+++ 98.validate-binary-search-tree.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=98 lang=cpp
+ *
+ * [98] Validate Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if (root == NULL) {
+            return true;
+        }
+
+        return check(root, LONG_MIN, LONG_MAX);
+    }
+
+    bool check(TreeNode* root, long min, long max) {
+        if (root == NULL) {
+            return true;
+        }
+        if (root->val <= min || root->val >= max) {
+            return false;
+        }
+        return check(root->left, min, root->val) && check(root->right, root->val, max);
+    }
+};
+// @lc code=end
+
