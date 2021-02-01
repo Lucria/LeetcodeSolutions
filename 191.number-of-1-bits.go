334dd377852c5e65a3b64855bec4d6ef217f51e9
Lucria <jerryzhang28eclipse@gmail.com>
2021-02-01 19:05:24 +0800

    Golang version of counting set bits

---
+++ 191.number-of-1-bits.go
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=191 lang=golang
+ *
+ * [191] Number of 1 Bits
+ */
+
+// @lc code=start
+func hammingWeight(num uint32) int {
+	count := 0
+	for num > 0 {
+		num &= (num - 1)
+		count++
+	}
+	return count
+}
+
+// @lc code=end
+
